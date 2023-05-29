58daf6c9f170c3f4934467fcfa2a62e737cf4709
Audouxh <hugo.audoux@epitech.eu>
2023-05-29 21:37:05 +0200

    add react native template app and clean App.js

---
+++ App.js
@@ -0,0 +1,23 @@
+import React from 'react';
+
+import {
+  SafeAreaView,
+  ScrollView,
+  StyleSheet,
+  Text,
+} from 'react-native';
+
+const App = () => {
+  return (
+    <SafeAreaView >
+      <ScrollView>
+        <Text>Wallet connection</Text>
+      </ScrollView>
+    </SafeAreaView>
+  );
+}
+
+const styles = StyleSheet.create({
+});
+
+export default App;
