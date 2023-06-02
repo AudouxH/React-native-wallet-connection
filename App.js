f8d8d90e924214ef4a42053052107656b0b65c6d
Audouxh <hugo.audoux@epitech.eu>
2023-06-02 22:35:45 +0200

    import @walletconnect/react-native-compat, @web3modal/react-native into app.js and create config file for web3Modal and can still build and run the app

--- App.js
+++ App.js
@@ -3,6 +3,11 @@ import React, { useRef, useState } from 'react';
 import ConnectedScreen from './src/screens/connectedScreen';
 import NotConnectedScreen from './src/screens/notConnectedScreen';
 
+// list of packages needed for web3Modal
+import '@walletconnect/react-native-compat';
+import {useWeb3Modal, Web3Button, Web3Modal} from '@web3modal/react-native';
+import {providerMetadata, sessionParams} from './src/constants/config';
+
 const App = () => {
   const actionSheetRef = useRef(null);
   const [userAddress, setUserAddress] = useState("");
