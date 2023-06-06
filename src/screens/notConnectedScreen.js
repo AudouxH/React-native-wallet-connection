5321c4613cf8a6e6f73bf62fbd639e08a3ce98de
Audouxh <hugo.audoux@epitech.eu>
2023-06-06 20:40:11 +0200

    implement double connection via Metamask and walletConnect but also can now scan a qrcode to connect manually

--- src/screens/notConnectedScreen.js
+++ src/screens/notConnectedScreen.js
@@ -1,17 +1,75 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 
 import {
+    Image,
     SafeAreaView,
     StyleSheet,
     Text,
     TouchableOpacity,
     View,
+    Linking
 } from 'react-native';
 
 import IonIcons from 'react-native-vector-icons/Ionicons';
 import ActionSheetAddress from '../components/ActionSheetAddress';
 
+// logo imports
+import logoMetamask from '../assets/metamask-logo.png';
+import logoWalletConnect from '../assets/walletconnect-logo.png';
+
+// list of packages needed for @metamask/sdk
+import MetaMaskSDK from '@metamask/sdk';
+import BackgroundTimer from 'react-native-background-timer';
+
+// list of packages needed for web3Modal
+import '@walletconnect/react-native-compat';
+import { useWeb3Modal, Web3Modal } from '@web3modal/react-native';
+import { providerMetadata, sessionParams } from '../constants/config';
+
 const NotConnectedScreen = ({ actionSheetRef, setUserAddress }) => {
+    const { isConnected, address, open, close, isOpen, provider } = useWeb3Modal();
+
+    const connectWithMetamask = async () => {
+        const MMSDK = new MetaMaskSDK({
+            openDeeplink: (link) => {
+                Linking.openURL(link);
+            },
+            timer: BackgroundTimer,
+            dappMetadata: {
+                name: 'walletConnection',
+                url: 'https://walletConnection.com',
+            },
+        });
+
+        const ethereum = MMSDK.getProvider();
+        const accounts = await ethereum.request({ method: 'eth_requestAccounts' });
+        console.log('RESULT', accounts?.[0]);
+        setUserAddress(accounts?.[0]);
+    }
+
+    // useEffect(() => {
+    //     if (isConnected && address) {
+    //         setUserAddress(address);
+    //         isOpen ? close() : null;
+    //     } else {
+    //         console.log("isConnected:", isConnected);
+    //         console.log("address:", address);
+    //     }
+    // }, [isConnected, address]);
+
+    // useEffect(() => {
+    //     async function getClientId() {
+    //         if (provider && isConnected) {
+    //             const _clientId = await provider?.client?.core.crypto.getClientId();
+    //             console.log("client id", _clientId);
+    //             console.log("address in client id:", address);
+    //         } else {
+    //             console.log("isConnected:", isConnected);
+    //         }
+    //     }
+
+    //     getClientId();
+    // }, [isConnected, provider]);
 
     return (
         <SafeAreaView style={styles.container}>
@@ -19,11 +77,16 @@ const NotConnectedScreen = ({ actionSheetRef, setUserAddress }) => {
                 <IonIcons name="wallet-outline" size={100} color="#2081e2" />
                 <Text style={styles.title}>Your wallet is not connected</Text>
                 <Text style={styles.subtitle}>Connect to any supported Wallet connect to have access to your data</Text>
-                <TouchableOpacity onPress={() => {
-
-                }} style={styles.button}>
-                    <Text style={styles.buttonText}>Connect wallet</Text>
-                </TouchableOpacity>
+                <View style={styles.buttonContainer}>
+                    <TouchableOpacity onPress={connectWithMetamask} style={styles.button}>
+                        <Image source={logoMetamask} style={styles.buttonLogo}/>
+                        <Text style={styles.buttonText}>Connect with Metamask</Text>
+                    </TouchableOpacity>
+                    <TouchableOpacity onPress={open} style={styles.button}>
+                        <Image source={logoWalletConnect} style={styles.buttonLogo}/>
+                        <Text style={styles.buttonText}>Connect with WalletConnect</Text>
+                    </TouchableOpacity>
+                </View>
                 <TouchableOpacity onPress={() => {
                     actionSheetRef.current?.show();
                 }} style={{ marginTop: 20 }}>
@@ -34,7 +97,13 @@ const NotConnectedScreen = ({ actionSheetRef, setUserAddress }) => {
 
             <ActionSheetAddress
                 actionSheetRef={actionSheetRef}
-                setUserAddress={setUserAddress}/>
+                setUserAddress={setUserAddress} />
+
+            <Web3Modal
+                projectId={"5c0e3814df7c19b9f153337997c46e15"}
+                providerMetadata={providerMetadata}
+                sessionParams={sessionParams}
+            />
         </SafeAreaView>
     );
 }
@@ -64,16 +133,27 @@ const styles = StyleSheet.create({
         textAlign: 'center',
         width: '90%',
     },
+    buttonContainer: {
+        width: '100%',
+        display: 'flex',
+        flexDirection: 'column',
+        justifyContent: 'space-around',
+        alignItems: 'center',
+    },
     button: {
-        width: '90%',
+        width: '80%',
         height: 50,
         borderRadius: 10,
         backgroundColor: '#2081e2',
         display: 'flex',
         flexDirection: 'row',
-        justifyContent: 'center',
+        justifyContent: 'space-evenly',
         alignItems: 'center',
-        marginTop: 60
+        marginTop: 20
+    },
+    buttonLogo: {
+        width: 30,
+        height: 30
     },
     buttonText: {
         fontSize: 16,
